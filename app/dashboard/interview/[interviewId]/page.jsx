731a155e85a6fad2f002a237389f43df93746de4
dilanchoksi <dilanchoksi@gmail.com>
2024-07-28 12:57:00 -0400

    querying db for id match

--- app/dashboard/interview/[interviewId]/page.jsx
+++ app/dashboard/interview/[interviewId]/page.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+"use client";
+import { db } from "@/utils/db";
+import { InterView } from "@/utils/schema";
+import { eq } from "drizzle-orm";
+import React, { useEffect } from "react";
 
 function Interview({ params }) {
-    const GetInterviewDetails = async() => {const result = await db.select().from(InterView)}
+  useEffect(() => {
+    GetInterviewDetails();
+  }, []);
+
+  const GetInterviewDetails = async () => {
+    const result = await db
+      .select()
+      .from(InterView)
+      .where(eq(InterView.mockId, params.interviewId));
+    console.log(result);
+  };
   return <div>Interview</div>;
 }
 
