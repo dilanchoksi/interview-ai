35d03ac7e02c91bfb0ec84a12934a8bfa38cf2a6
dilanchoksi <dilanchoksi@gmail.com>
2024-07-28 22:15:01 -0400

    fixed some routing

--- app/page.js
+++ app/page.js
@@ -1,3 +1,14 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
-  return <div>Test page (home)</div>;
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/dashboard");
+  }, [router]);
+
+  return;
 }
