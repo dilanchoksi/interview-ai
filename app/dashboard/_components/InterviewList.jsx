6e17eead4eb372e6139c99b72391a0730cc69b74
dilanchoksi <dilanchoksi@gmail.com>
2024-08-02 18:35:56 -0400

    fixed alert dialog description

--- app/dashboard/_components/InterviewList.jsx
+++ app/dashboard/_components/InterviewList.jsx
@@ -106,9 +106,7 @@ function InterviewList() {
                         Are you absolutely sure?
                       </AlertDialogTitle>
                       <AlertDialogDescription>
-                        This action cannot be undone. This will permanently
-                        delete your account and remove your data from our
-                        servers.
+                        This action cannot be undone.
                       </AlertDialogDescription>
                     </AlertDialogHeader>
                     <AlertDialogFooter>
